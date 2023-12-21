83d99f68dbb333ba3d7fb609783bb0ad649dba49
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-21 14:30:55 +0300

    feat: module4 task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = (first >= second && first >= third) ?
+            first :
+            (second >= first && second >= third) ?
+                second : third;
+
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        new TernaryMaxNumberPrinter().printGreatest(5,2,3);
     }
 }
